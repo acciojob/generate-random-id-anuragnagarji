154c07de95ec0b885b3e88be3cfc88133b01e100
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-30 23:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function makeid(l) {
-  // write your code here
+    let str="";
+    while(str.length!=l){
+     let temp =Math.ceil(Math.random()*130);
+    if((temp>=48&&temp<=57)||(temp>=65&&temp<=91)||(temp>=97&&temp<=122)){
+      str+=String.fromCharCode(temp);
+      //console.log(temp+" "+str);
+    }
+    
 }
+  return str;
+  }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
